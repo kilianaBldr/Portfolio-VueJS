90669568e736851653ab965d94552223121e7e3d
KilianaBldr <kiliana.bldr@gmail.com>
2024-05-29 19:21:12 +0200

    Ajout de la partie About

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,63 @@
+<template>
+    <section id="presentation">
+        <div class="texte">
+            <p>Et la cest moi !</p>
+            <h1>Kiliana Blondron</h1>
+            <p>Vous vous demandez surement qui je suis !</p>
+            <p>Derrière cet écran se cache une jeune développeuse en herbe, passionnée par le monde numérique depuis toujours.L'informatique est mon terrain de jeu, où je jongle avec les codes et les algorithmes avec un enthousiasme sans bornes. </p>
+            <p>Un brin geek sur les bords, j'assume pleinement mon côté "accro aux technologies". Mais ne vous y trompez pas, je ne suis pas qu'une machine à calculer ! J'ai aussi un bon sens de l'humour et j'aime prendre la vie du bon côté.</p>
+            <p>Mon rêve ? Devenir une développeuse accomplie, capable de créer des outils innovants et utiles qui impactent positivement le monde. Je suis déterminée, travailleuse et toujours à l'affût de nouvelles connaissances.</p>
+            <p>Alors, si vous cherchez une développeuse enthousiaste, créative et avec une touche d'humour, n'hésitez pas à me contacter. Je suis impatiente de partager ma passion avec vous et de relever de nouveaux défis ensemble !</p>
+            <h2>Mes Compétences</h2>
+            <p>HTML 5</p>
+            <p>CSS 3</p>
+            <p>Javascript</p>
+        </div>
+        <img src="../assets/img/ImageAbout.jpg" alt="">
+    </section>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Aclonica&family=Audiowide&display=swap');
+
+section {
+    background-image: url('https://wallpaperaccess.com/full/722735.png');
+    border-radius: 10px;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    font-family: "Aclonica", sans-serif;
+    display: flex;
+    justify-content: space-around;
+    margin-top: 80px;
+}
+
+.texte {
+    background-color: rgba(5, 13, 26, 0.523);
+    border-radius: 10px;
+    color: white;
+    width: 50%;
+    margin: 15px;
+    padding: 30px;
+    letter-spacing: 1px;
+}
+
+h2 {
+    font-size: 17px;
+}
+
+p {
+    font-size: 12px;
+}
+
+img {
+    border-radius: 0px 50% 50% 50%;
+    height: 250px;
+    width: 250px;
+    margin-top: 100px;
+}
+
+</style>
